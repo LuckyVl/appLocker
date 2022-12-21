dcb4a98c81c9bfbb75b9c8ada2a4c554a1fd0a0a
Vladimir <94462542+LuckyVl@users.noreply.github.com>
2022-12-21 13:16:02 +0300

    ropo(v2)

--- ropo.py
+++ ropo.py
@@ -1,14 +1,22 @@
-import shutil, re
+import shutil, re, os
 
 list1 = []
 list2 = []
+# считываем адреса откуда и куда
 with open("resurse.txt") as file:
     for item in file:
         list1.append(item)
 # print(list1)
 file.close()
+# чистим адреса
 for i in list1:
     i = re.sub(r"\n","",i)
     list2.append(i)
 # print(list2)
-shutil.copy(str(list2[0]),str(list2[1]))
+pathout = list2[0]
+# print(pathout)
+temp = pathout.split("\\")
+# print(temp)
+pathin = list2[1] + temp[-2]
+# print(pathin)
+shutil.copytree(pathout,pathin)
